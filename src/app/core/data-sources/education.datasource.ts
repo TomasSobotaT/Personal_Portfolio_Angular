f3c00f08db8eb68eeff73771cc69d1dd0267dd5c
Tomas Sobota <tomas.sobota@verisoft.cz>
2025-12-08 23:54:23 +0100

    fix details

--- src/app/core/data-sources/education.datasource.ts
+++ src/app/core/data-sources/education.datasource.ts
@@ -3,25 +3,28 @@ import { Project } from '../models/project.model';
 export const EDUCATIONS: Project[] = [
   {
     id: '1',
-    period: 'říjen 2025',
+    period: '10/2025',
     title: 'Microsoft Azure – The Complete Guide',
     description: 'Kompletní kurz Microsoft Azure na Udemy.',
     tags: ['Microsoft Azure', 'Cloud'],
+    imageUrl: '-',
   },
   {
     id: '2',
-    period: 'září 2025',
+    period: '09/2025',
     title: 'Angular – The Complete Guide (2025 Edition)',
     description: 'Komplexní kurz Angularu a Typescriptu na Udemy.',
     tags: ['Angular', 'Typescript', 'Frontend'],
+    imageUrl: '-',
   },
   {
     id: '3',
-    period: 'červenec 2025',
+    period: '07/2025',
     title: 'Optimalizace výkonu, ladění a škálovatelnost aplikací',
     description:
       'Profesionální školení Gopas zaměřené na výkon, ladění a škálování .NET aplikací.',
     tags: ['Škálovatelnost', '.NET', 'Optimalizace výkonu'],
+    imageUrl: '-',
   },
   {
     id: '4',
@@ -30,5 +33,6 @@ export const EDUCATIONS: Project[] = [
     description:
       'Kurz C#, ASP.NET, ADO.NET, EF, MS-SQL, Javascript, HTML, CSS, Bootstrap, Git.',
     tags: ['C#', 'Javascript ', 'HTML', 'CSS'],
+    imageUrl: '-',
   },
 ];
