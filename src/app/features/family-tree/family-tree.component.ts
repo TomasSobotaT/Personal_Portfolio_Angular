8039b07ede99e7041b14a46113a9e99765212b22
Tomas Sobota <tomas.sobota@verisoft.cz>
2025-12-20 00:23:31 +0100

    add footers

--- src/app/features/family-tree/family-tree.component.ts
+++ src/app/features/family-tree/family-tree.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { FooterBarComponent } from '../../shared/footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-family-tree',
-  imports: [FooterComponent],
+  imports: [FooterComponent, FooterBarComponent],
   templateUrl: './family-tree.component.html',
   styleUrl: './family-tree.component.scss',
 })
-export class FamilyTreeComponent {}
+export class FamilyTreeComponent { }
--- src/app/features/helpers/helpers.component.ts
+++ src/app/features/helpers/helpers.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HELPERS } from '../../core/data-sources/helper.datasource';
+import { FooterBarComponent } from '../../shared/footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-helpers',
   standalone: true,
+  imports: [FooterBarComponent],
   templateUrl: './helpers.component.html',
   styleUrls: ['./helpers.component.scss'],
 })
