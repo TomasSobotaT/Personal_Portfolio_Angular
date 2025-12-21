e1b6f600005801790dd54d7cdf36c8ca3c6a5973
Tomas Sobota <tomas.sobota@verisoft.cz>
2025-12-21 13:52:24 +0100

    add new datasource

--- src/app/core/data-sources/education.datasource.ts
+++ src/app/core/data-sources/education.datasource.ts
@@ -3,12 +3,21 @@ import { Project } from '../models/project.model';
 export const EDUCATIONS: Project[] = [
   {
     id: '1',
-    period: '10/2025',
-    title: 'Microsoft Azure – The Complete Guide',
-    description: 'Kompletní kurz Microsoft Azure na Udemy.',
-    tags: ['Microsoft Azure', 'Cloud'],
-    link: 'https://www.udemy.com/course/microsoft-azure-from-zero-to-hero-the-complete-guide',
+    period: '12/2025',
+    title: 'Kompletní administrace Microsoft SQL Serveru',
+    description:
+      'Komplexní kurz administrace Microsoft SQL Serveru: instalace, konfigurace, zabezpečení, údržba, zálohování, obnova a monitoring.',
+    tags: ['MS-SQL', 'T-SQL', 'Databáze'],
+    link: 'https://www.gopas.cz/kompletni-administrace-microsoft-sql-serveru_goc6345',
   },
+  // {
+  //   id: '1',
+  //   period: '10/2025',
+  //   title: 'Microsoft Azure – The Complete Guide',
+  //   description: 'Kompletní kurz Microsoft Azure na Udemy.',
+  //   tags: ['Microsoft Azure', 'Cloud'],
+  //   link: 'https://www.udemy.com/course/microsoft-azure-from-zero-to-hero-the-complete-guide',
+  // },
   {
     id: '2',
     period: '09/2025',
@@ -24,7 +33,7 @@ export const EDUCATIONS: Project[] = [
     description:
       'Profesionální školení Gopas zaměřené na výkon, ladění a škálování .NET aplikací.',
     tags: ['.NET', 'Optimalizace', 'Ladění'],
-    link: 'https://www.gopas.cz/skoleni/optimalizace-vykonu-ladeni-a-skalovatelnost-aplikaci-dotnet',
+    link: 'https://www.gopas.cz/optimalizace-vykonu-ladeni-a-skalovatelnost-aplikaci_goc2126',
   },
   {
     id: '4',
